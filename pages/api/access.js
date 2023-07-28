326f7643727525692e8406fa4b6363cfc47942ef
CustomHaven <mmuseaden@gmail.com>
2023-07-28 02:16:52 +0100

    take 13

--- pages/api/access.js
+++ pages/api/access.js
@@ -5,43 +5,45 @@ export default async function POST(req, res) {
     // process.env.BACKEND
 
     try {
-        console.log("inside the /api/refresh");
-        console.log(req.body.email);
-        // console.log(req.body.refresh_token);
-        console.log("req.body.email");
-
-        const response = await fetch("https://api-custom-ecommerce-pern.onrender.com/api/v2/auth/login", {
-        // const response = await fetch("http://localhost:5000/api/v2/auth/login", {
-            method: "POST",
-            headers: {
-                "Accept": "application/json",
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({ email: req.body.email, password: req.body.password, frontend: "incoming frontend" }),
-            credentials: "include"
-        });
-
+        if (req.method === "POST") {
+            console.log("inside the /api/refresh");
+            console.log(req.body.email);
+            // console.log(req.body.refresh_token);
+            console.log("req.body.email");
     
-        const data = await response.json();
-
-        console.log("inside the /api/refresh looking at data we got?");
-        console.log(data);
-        console.log("inside the /api/refresh looking at data we got?");
-        console.log("res");
-        console.log(res);
-        console.log("res");
+            const response = await fetch("https://api-custom-ecommerce-pern.onrender.com/api/v2/auth/login", {
+            // const response = await fetch("http://localhost:5000/api/v2/auth/login", {
+                method: "POST",
+                headers: {
+                    "Accept": "application/json",
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({ email: req.body.email, password: req.body.password, frontend: "incoming frontend" }),
+                credentials: "include"
+            });
     
-        res
-            .setHeader("Set-Cookie", [
-                serialize("token_id", data.refresh_token, { path: "/", httpOnly: true, maxAge: data.expiration, /* secure: true, sameSite: "lax" */ }),
-                serialize("access_token", data.access_token, { path: "/", httpOnly: true, maxAge: data.expiration, /* secure: true, sameSite: "lax" */ })
-            ])
-            .status(201)
-            .json(JSON.stringify({ message: "Access log in successfully 😊 👌", user: data.user, token: data.access_token, refresh_token: data.refresh_token }))
-            .end(res.getHeader('Set-Cookie'))
-            // .end();
+        
+            const data = await response.json();
     
-        // res.status(200).end();
+            console.log("inside the /api/refresh looking at data we got?");
+            console.log(data);
+            console.log("inside the /api/refresh looking at data we got?");
+            console.log("res");
+            console.log(res);
+            console.log("res");
+        
+            res
+                .setHeader("Set-Cookie", [
+                    serialize("token_id", data.refresh_token, { path: "/", httpOnly: true, maxAge: data.expiration, /* secure: true, sameSite: "lax" */ }),
+                    serialize("access_token", data.access_token, { path: "/", httpOnly: true, maxAge: data.expiration, /* secure: true, sameSite: "lax" */ })
+                ])
+                .status(201)
+                .json(JSON.stringify({ message: "Access log in successfully 😊 👌", user: data.user, token: data.access_token, refresh_token: data.refresh_token }))
+                .end(res.getHeader('Set-Cookie'))
+                // .end();
+        
+            // res.status(200).end();
+        }
 
     } catch (error) {
         res.status(500).json({ message: "failed to load data" })
--- pages/api/refresh.js
+++ pages/api/refresh.js
@@ -5,48 +5,51 @@ export default async function POST(req, res) {
     // process.env.BACKEND
 
     try {
-        console.log("inside the /api/refresh");
-        console.log(req.body.refresh_token);
-        console.log("inside the /api/refresh");
-
-        console.log("req.body.refresh_token");
-        console.log(req.body.refresh_token);
-        console.log("req.body.refresh_token");
-
-
-        const response = await fetch("https://api-custom-ecommerce-pern.onrender.com/api/v2/auth/refresh", {
-        // const response = await fetch("http://localhost:5000/api/v2/auth/refresh", {
-            method: "POST",
-            headers: {
-                "Accept": "application/json",
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({ refresh_token: req.body.refresh_token, frontend: "incoming frontend" }),
-            // credentials: "include"
-        });
+        if (req.method === "POST") {
+            console.log("inside the /api/refresh");
+            console.log(req.body.refresh_token);
+            console.log("inside the /api/refresh");
     
-        const data = await response.json();
+            console.log("req.body.refresh_token");
+            console.log(req.body.refresh_token);
+            console.log("req.body.refresh_token");
+    
+    
+            const response = await fetch("https://api-custom-ecommerce-pern.onrender.com/api/v2/auth/refresh", {
+            // const response = await fetch("http://localhost:5000/api/v2/auth/refresh", {
+                method: "POST",
+                headers: {
+                    "Accept": "application/json",
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({ refresh_token: req.body.refresh_token, frontend: "incoming frontend" }),
+                // credentials: "include"
+            });
+        
+            const data = await response.json();
+    
+            console.log("inside the /api/refresh looking at data we got?");
+            console.log(data);
+            console.log("inside the /api/refresh looking at data we got?");
+    
+    
+            console.log("res");
+            console.log(res);
+            console.log("res");
+    
+    
+            res
+                .setHeader("Set-Cookie", [
+                    serialize("access_token", "", { path: "/", httpOnly: true, maxAge: 0 }),
+                    serialize("token_id", data.token, { path: "/", httpOnly: true, maxAge: data.expiration, /* secure: true, sameSite: "lax" */ }),
+                    serialize("refreshed_token", data.refresh_token, { path: "/", httpOnly: true, maxAge: data.expiration, /* secure: true, sameSite: "lax" */ })
+                ])
+                .status(201)
+                .json(JSON.stringify({ message: "refresh token done!", user: data.user, token: data.refresh_token, refresh_token: data.token }))
+                .end(res.getHeader('Set-Cookie'))
+                // .end();
+        }
 
-        console.log("inside the /api/refresh looking at data we got?");
-        console.log(data);
-        console.log("inside the /api/refresh looking at data we got?");
-
-
-        console.log("res");
-        console.log(res);
-        console.log("res");
-
-
-        res
-            .setHeader("Set-Cookie", [
-                serialize("access_token", "", { path: "/", httpOnly: true, maxAge: 0 }),
-                serialize("token_id", data.token, { path: "/", httpOnly: true, maxAge: data.expiration, /* secure: true, sameSite: "lax" */ }),
-                serialize("refreshed_token", data.refresh_token, { path: "/", httpOnly: true, maxAge: data.expiration, /* secure: true, sameSite: "lax" */ })
-            ])
-            .status(201)
-            .json(JSON.stringify({ message: "refresh token done!", user: data.user, token: data.refresh_token, refresh_token: data.token }))
-            .end(res.getHeader('Set-Cookie'))
-            // .end();
 
     } catch (error) {
         res.status(500).json(JSON.stringify({ message: "failed to load data" }))
