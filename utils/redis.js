b5a9bd2e217ea69f85f636666c93940737a843c5
CustomHaven <mmuseaden@gmail.com>
2023-08-09 22:28:18 +0100

    take 4

--- utils/redis.js
+++ utils/redis.js
@@ -1,8 +1,10 @@
 import Redis from "ioredis";
 console.log("okat?!!")
 console.log("process.env.REDIS_CACHING", process.env.REDIS_CACHING);
+console.log("process.env.NODE_ENV", process.env.NODE_ENV);
 let redis;
 if (process.env.NODE_ENV === "production") {
+    console.log("IN PRODUCTION! process.env.REDIS_CACHING", process.env.REDIS_CACHING);
     redis = new Redis(process.env.REDIS_CACHING);
 } else {
     redis = new Redis();
